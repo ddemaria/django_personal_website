e92b01eb1d36fed0ba2201c94323ff3e8dc0d730
ddemaria <ddemaria4@gmail.com>
2023-02-18 07:56:09 +0000

    fixing static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,15 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/static/'
-MEDIA_URL = '/static/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "app/core/static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
