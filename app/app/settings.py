e803f1a1f8a50d8c4b168f5136a1d71a82262103
ddemaria <ddemaria4@gmail.com>
2023-02-18 08:05:33 +0000

    fix static files for deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = '/media/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
